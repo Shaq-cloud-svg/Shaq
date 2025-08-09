25b2036a05ac5b4de3a5fad2365f5d834d6bc038
Shaq <27349578@sun.ac.za>
2025-08-09 15:21:00 +0200

    we installed pyhon and ran a new code and debug it

---
+++ hello.py
@@ -0,0 +1,13 @@
+from datetime import date
+
+def say_hello(name):
+    print("Hello, " + name)
+
+say_hello("VS Code")
+
+def say_day_of_week(date):
+    print("Today is " + str(date))
+
+say_day_of_week(date.today())
+
+    
